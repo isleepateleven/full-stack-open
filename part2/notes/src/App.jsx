5400c7b82776e250638b5518f6e31429b6f9519c
isleepateleven <phoebeting11@gmail.com>
2025-03-07 23:18:54 +0800

    Completed Part 2 Exercises 2.12-2.15

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,38 +1,53 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
-const App = (props) => {
+const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
-  // executed immediately after rendering
-  // param 1: function (effect itself)
-  // param 2:  specify how often the effect is run ([] means only run along with the first render)
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes') // fetching of data from the server
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data) // triggers the re-rendering of the component
+    noteService
+      .getAll()
+      .then((initialNotes) => {
+        setNotes(initialNotes)
       })
   }, [])
-  console.log('render', notes.length, 'notes')
 
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    }
+
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    })
+  }
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
+      })
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server`)
+        setNotes(notes.filter((n) => n.id !== id))
+      })
+  }
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
 
-  const addNote = (event) => {
-    event.preventDefault()
-  }
-
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <div>
@@ -43,16 +58,20 @@ const App = (props) => {
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
-        )}
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
       </ul>
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
-      </form>   
+      </form>
     </div>
   )
 }
 
-export default App 
+export default App
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,13 +11,84 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect( () => {
-    axios
-      .get('http://localhost:3002/persons')
-      .then(response =>{
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then((initialPersons)=>{
+        setPersons(initialPersons)
       })
   }, [])
-  
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    // Check if there is existing person 
+    const existingPerson = persons.find(
+      person => person.name.toLowerCase() === newName.toLowerCase()
+    )
+    
+    // If user exists, replace the old number 
+    if (existingPerson){
+      const confirmUpdate = window.confirm(
+        `${newName} is already added to phonebook, replace the old number with a new one?`
+      );
+
+      if (confirmUpdate){
+        const updatedPerson = { ...existingPerson, number: newNumber };
+
+        // Send PUT request to update the number
+        personService
+          .update(existingPerson.id, updatedPerson)
+          .then((returnedPerson) => {
+            setPersons(persons.map((person) => 
+              person.id !== existingPerson.id ? person : returnedPerson
+            ));
+            setNewName("");
+            setNewNumber("");
+          })
+          .catch(error => {
+            alert(`Error: ${newName} was already deleted from the server.`);
+            setPersons(persons.filter(person => person.id !== existingPerson.id)); 
+          });
+                    
+      }
+      return;
+    } 
+    
+    // If name doesn't exist, create new contact
+    const personObj = {
+      name: newName,
+      number: newNumber,
+      // id: persons.length + 1
+    }
+
+    personService
+      .create(personObj)
+      .then((returnedPersons) => {
+        setPersons(persons.concat(returnedPersons))
+        setNewName("")
+        setNewNumber("")
+      })
+  }
+
+  const deletePerson = (id, name) => {
+      if(window.confirm(`Delete ${name} ?`)){
+        personService.remove(id)
+          .then(() => {
+            setPersons(persons.filter((p) => p.id !== id)) // keeps only persons whose id is NOT the deleted one.
+          })
+          .catch(error => {
+            alert(`Error: ${name} was already deleted from server`)
+            setPersons(persons.filter((p) => p.id !== id)) 
+          })
+      }
+  }
+
+  const personsToShow = filter
+    ? persons.filter(person => person.name.toLowerCase()
+        .includes(filter.toLowerCase())) //checks if the name contains the search term
+    : persons
+
+    
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -29,37 +100,6 @@ const App = () => {
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
   }
-
-
-  const addPerson = (event) => {
-    event.preventDefault()
-
-    // Check if names exist
-    const isNameExist = persons.some(
-      person => person.name.toLowerCase() === newName.toLowerCase()
-    )
-    
-    // Prevent the user from being able to add names that already exist 
-    if (isNameExist){
-      alert(`${newName} is already added to phonebook`)
-      return
-    } 
-    
-    const personObj = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1
-    }
-
-    setPersons([...persons, personObj])
-    setNewName("")
-    setNewNumber("")
-  }
-
-  const personsToShow = filter
-    ? persons.filter(person => person.name.toLowerCase()
-        .includes(filter.toLowerCase())) //checks if the name contains the search term
-    : persons
 
   return (
     <div>
@@ -76,7 +116,7 @@ const App = () => {
       />
 
       <h2>Numbers</h2>
-      <Persons personsToShow={personsToShow} />
+      <Persons personsToShow={personsToShow} deletePerson={deletePerson}/> 
     </div>
   )
 }
