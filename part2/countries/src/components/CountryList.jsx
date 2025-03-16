42c78c69e0ddd6960cc9c3f25ef7cb520690b625
isleepateleven <phoebeting11@gmail.com>
2025-03-17 00:05:50 +0800

    Completed Part 2 Exercises 2.19

--- part2/countries/src/components/CountryList.jsx
+++ part2/countries/src/components/CountryList.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
 import CountryDetails from './CountryDetails'
 
 const CountryList = ({ countries, filter }) => {
+    const [selectedCountry, setSelectedCountry] = useState(null)
+    
     if (!filter){
         return <div></div>
     }
-    
+
     if (countries.length > 10) {
         return <div>Too many matches, specify another filter</div>
     }
@@ -13,11 +16,17 @@ const CountryList = ({ countries, filter }) => {
         return <CountryDetails country={countries[0]} />
     }
 
+    const handleShowClick = (country) => setSelectedCountry(country)
+
     return (
         <div>
-            {countries.map( country =>
-                <div key={country.cca3}> {country.name.common} </div>
+            {countries.map(country =>
+                <div key={country.cca3}> 
+                    {country.name.common} <button onClick={() => handleShowClick(country)}>Show</button>
+                </div>
             )}
+
+            {selectedCountry && <CountryDetails country={selectedCountry} />}
         </div>
     )
 }
